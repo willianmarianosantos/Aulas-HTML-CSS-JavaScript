1b37dba08fbe839ef00a937a834b2bdd062982ff
Willian Mariano <112572537+willianmarianosantos@users.noreply.github.com>
2023-10-10 14:33:45 -0300

    Add files via upload

---
+++ JS+HTML+CSS001/JavaScript.js
@@ -0,0 +1,35 @@
+﻿let colocacao = 4
+switch (colocacao) {
+    case 1: console.log("Primeiro Lugar disparado")
+        break    
+    case 2: console.log("Segundo  Lugar")
+        break   
+    case 3: case 4: console.log("Premio de participacao")
+        break
+    default:
+        console.log("Nao subiu ao podio")
+        break
+}
+
+
+
+
+const objs1 = [...document.getElementsByClassName("m")]   // esta forma de mudar a classe percorrento todas com o mesmo nome, uso de spread ...
+objs1.forEach(Element => { Element.innerHTML = "testedeCLASSE" });
+console.log(objs1) // para mostrar dentro do console
+
+
+
+document.getElementById("B").innerHTML = "TesteID"; // uma das formas de mudar o id, mas somente 1 por linha
+
+
+
+
+let b2 = document.getElementById("C") // uma das formas de mudar o id, mas somente 1 por linha
+    b2.innerHTML="TesteID2"
+
+
+
+
+
+
